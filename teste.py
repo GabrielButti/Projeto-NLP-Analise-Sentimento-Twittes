e24278a38318229e415eed79198fdfb00f4f4fc7
Gabriel Butti <gabriel.butti@hotmail.com>
2025-10-16 19:30:48 -0300

    Exclusão de arquivo de teste: teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_URL = "http://localhost:8000"
-text = "I love it!"
-
-resp = requests.post(f"{API_URL}/analise/", json={"texto": text})
-print("Status code:", resp.status_code)
-print("Response JSON:", resp.json())
